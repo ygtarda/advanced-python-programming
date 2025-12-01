e91757db95a9448fba7760c648137e5038a1b02c
Arda <ardayig725@gmail.com>
2025-12-02 01:35:24 +0300

    Update strings.py

--- Strings/strings.py
+++ Strings/strings.py
@@ -3,8 +3,5 @@ surname = "Yiğit"
 age = 19
 
 print("My name is " + name + " " + surname + " and \nI am " + str(age) + " years old.")
-print(f"My name is {name} {surname} and I am {age} years old.")
-print("My name is {} {} and I am {} years old.".format(name, surname, age))
-
 
 
